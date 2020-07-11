30b094bfb8491b75f41720ec2ef84c8120ea6f06
Sam Smith <dfc@g.com>
2020-07-11 13:56:11 +0530

    part of branch 2

---
+++ src/TestCases/ProfileTest.java
@@ -0,0 +1,12 @@
+package TestCases;
+
+public class ProfileTest {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("profile test");
+
+	}
+
+}
---
+++ src/TestCases/Reservation.java
@@ -0,0 +1,11 @@
+package TestCases;
+
+public class Reservation {
+
+	public static void main(String[] args) {
+		
+		System.out.println("reservation");
+
+	}
+
+}
